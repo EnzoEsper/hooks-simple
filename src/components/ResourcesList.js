b3a180207f0cddc67e92a475effd15eaf65cf02e
enzoesper <enzoesper@hotmail.com>
2019-07-07 15:19:00 -0300

    refactoring the class based component to a functional componetn

--- src/components/ResourcesList.js
+++ src/components/ResourcesList.js
@@ -1,29 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourcesList extends React.Component {
+const ResourcesList = () => {
   
-  state = { resources:[] };
-
-  async componentDidMount(){
+  const [resources, setResources] = useState([]);
+  
+  const fetchResource = async () => {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
 
     this.setState({ resources: response.data });
-  }
-
-  async componentDidUpdate(prevProps){
-    if (prevProps.resource !== this.props.resource) {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-
-      this.setState({ resources: response.data });
-    }
-  }
-
-  render(){
-    return(
-      <div>{this.state.resources.length}</div>
-    )
   };
+
+  return(
+    <div>{resources.length}</div>
+  );
+
 };
 
 export default ResourcesList;
